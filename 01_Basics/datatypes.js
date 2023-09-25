93d13e4d602cb0e857d3c9cdc41d4940db01cec8
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2023-09-25 08:25:11 +0000

    datatypes in js

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,22 @@
+/*
+Data Types 
+
+Numbers => range upto 2^53
+bigInt
+Boolean => True/False
+String => "Qaisar"
+null => stand alone value 
+undefine => value of uninitilized variable 
+symbol
+*/
+
+// Object
+//It is a object initilization 
+let qaisar = {   qaisar1 :43,     
+    meer:44 , abbujan:99,ammi:99}
+// console.log(qaisar)
+
+// console.log(typeof(qaisar)) 
+//typeof() will print ,what type of object is variable 
+
+
--- 01_Basics/first.js
+++ 01_Basics/first.js
@@ -3,4 +3,13 @@ let accountEmail = "qaisarashraf685@gmail.com"
 let accountPasssword = 989         //Make a habit of using let in javascript 
 let accountCity = "Jabalpur"
 
-console.table([accountEmail,accountCity,accountId,accountPasssword])
+// console.table([accountEmail,accountCity,accountId,accountPasssword])
+//console.table([]) it will print all the elements in tabular form 
+//The value of uninitilized variable is "Undefine"
+
+
+
+
+
+
+
