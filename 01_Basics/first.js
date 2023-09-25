8aa7ef4e1e80f9ec166ffdc7ffcdab532f8cebe8
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2023-09-25 07:58:29 +0000

    first program

--- 01_Basics/first.js
+++ 01_Basics/first.js
@@ -1,1 +1,6 @@
-console.log("Qaisar")
+const accountId = 123456 //This cannot be changed 
+let accountEmail = "qaisarashraf685@gmail.com"
+let accountPasssword = 989         //Make a habit of using let in javascript 
+let accountCity = "Jabalpur"
+
+console.table([accountEmail,accountCity,accountId,accountPasssword])
