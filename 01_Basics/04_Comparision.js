a20e3a6353ce1a09b78870121c0a939593cc7033
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2023-10-01 07:58:15 +0000

    more code

--- 01_Basics/04_Comparision.js
+++ 01_Basics/04_Comparision.js
@@ -6,9 +6,16 @@
 // console.log(2 == 1)
 // console.log(2 === 1) //check strictly (check data type also)
 
-//Problem Starts Here
+//********************Problem Starts Here********************
 //When we compare two different data type 
+//Dont't do this it not gove always a predictable answers 
+
 
 // console.log("2" > 1) // we are comparing string with number.
 //^^ first it will convert string to number
 // console.log("02" > 1)
+
+// console.log(null > 0) //output false
+// console.log(null == 0) //output false
+// console.log(null >= 0) //output true
+// ************************************************************
