32f603bad423f2ea2191f14b3d954bb8cca0c6aa
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2023-10-03 05:09:22 +0000

    memory stack

--- 01_Basics/data_type_summary.js
+++ 01_Basics/data_type_summary.js
@@ -11,8 +11,8 @@ let qaisar; //default value is undefined
 let Id = Symbol('1234') // The value of both of them is same but both 
 let newId = Symbol('1234') //. of them are different to each other 
                           //The data type of return value is Symbol
-console.log(Id === newId)//for cross verifying is both are same or not
-console.log(Id,newId);
+// console.log(Id === newId)//for cross verifying is both are same or not
+// console.log(Id,newId);
 
 
 // Non-Primitive(Refrence ,Call By Address)
@@ -29,3 +29,37 @@ const myObj = {
 const myFunction = function qaisar(){
     console.log("Hello world");
 }
+ 
+
+
+//******************************************************//
+//Types of memory[ Stack(primitive),Heap(non-primitive)]
+// 1)Stack memory used by primitive datatype
+
+let myYoutubeName = "Mohd Qaisar Moin"
+
+let stacktrial = myYoutubeName //it will provide a copy of myYoutubeName not the actual value of it.
+
+stacktrial = "Meer Ashraf" //it will change only the value of satcktrial not the value of myYoutubeName
+
+// console.log(myYoutubeName)
+//Here we see that the value of only stacktrial get changed
+// console.log(stacktrial)
+
+
+
+// 2)Heap memory used by non-primitive datatype 
+
+let userOne = {
+    email:"qaisarashraf685@gmail.com",
+    password: 9893676520
+}
+
+let userTwo = userOne
+
+userTwo.email = "meerashraf685@gmail.com"
+
+// console.log(userOne.email);
+//Here we can see that the value of both of them were get changed.
+// console.log(userTwo.email);
+
