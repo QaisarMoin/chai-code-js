9e3667751983fc0e38acbd72b909ff51bea7ed5d
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2023-10-03 07:25:18 +0000

    i din't know

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -2,8 +2,20 @@
 
 let firstName = "Qaisar"
 let lastName = " Moin"
+let repoCount = 50
 
 // console.log("MY name is "+firstName+lastName);
 
 //It is the old way ,we don't use this method 
 
+//the new is down below 
+
+
+console.log(`Hello My name is ${firstName} and my repo count is ${repoCount}`);//this is mast way to write a string 
+
+
+
+
+let url = "qaisar monin is qaisar moin "
+let url2 = url.replace('qaisar','meer')
+console.log(url2);
